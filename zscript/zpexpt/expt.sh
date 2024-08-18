25acd2ed2610b01f1f210d4d7f562114f9ec27c7
liangbo6 <3548673252@qq.com>
2024-08-18 18:29:32 +0800

    my_first

--- zscript/zpexpt/expt.sh
+++ zscript/zpexpt/expt.sh
@@ -14,8 +14,23 @@ helpz(){
 	return 0
 }
 
+checkFile(){
+	if [ -f $1 ];then
+		while true
+		do
 
-
+			echo -n "File $1 already exists,Do you want to overwrite it?[y/n]: "
+			read choice
+			if [ "${choice}" = 'yes' ] || [ "${choice}" = 'y' ] || [ "${choice}" = 'Y' ] || [ "${choice}" = "YES" ];then
+				break
+			elif [ "${choice}" = 'no' ] || [ "${choice}" = 'n' ] || [ "${choice}" = 'N' ] || [ "${choice}" = "NO" ];then
+				exit -1
+			else 
+				continue
+			fi 
+		done
+	fi
+}
 
 case $1 in
 	-h|--help)
@@ -25,10 +40,12 @@ case $1 in
 	--)
 		shift
 		if [ $# == 0 ];then
+			checkFile "$(pwd)/exp.py"
 			`cp /home/liangbo/tools/zscript/zpexpt/expt.py $(pwd)/exp.py`
 		else
 			until [ $# == 0 ]
 			do
+				checkFile "$(pwd)/$1"
 				`cp /home/liangbo/tools/zscript/zpexpt/expt.py $(pwd)/$1`
 			shift
 			done
@@ -41,6 +58,4 @@ case $1 in
 		exit 1
 		;;
 esac
-
-
 
